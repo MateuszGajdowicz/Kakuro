eba2df19535711de8b6275678be053805f7b88c1
matsz <01178640@pw.edu.pl>
2023-06-14 14:43:07 +0200

    Merge remote-tracking branch 'origin/main'
    # Conflicts:
    #	src/main/java/org/projekt/GUI.java

---
+++ src/main/java/org/projekt/SaveInfo.java
@@ -0,0 +1,17 @@
+package org.projekt;
+
+import java.io.*;
+
+public class SaveInfo implements Serializable {
+
+    static public void saveObj(GUI obj) throws IOException {
+
+        FileOutputStream fileOut = new FileOutputStream("SavedBoard.ser");
+        ObjectOutputStream out = new ObjectOutputStream(fileOut);
+
+        out.writeObject(obj);
+
+        fileOut.close();
+        out.close();
+    }
+}
