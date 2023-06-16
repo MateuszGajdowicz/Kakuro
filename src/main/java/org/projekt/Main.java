77c2afd5e184b4015798e75c766a02ccc510acb7
iszczygi <01178696@pw.edu.pl>
2023-06-16 07:59:38 +0200

    dodanie odpalenia z Main

--- src/main/java/org/projekt/Main.java
+++ src/main/java/org/projekt/Main.java
@@ -5,14 +5,17 @@ import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        Board board = new Board(15,15);
-        VisualizerAscii v = new VisualizerAscii(board);
-
-
-        List<Integer> list= Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9);
-        for(int i = 0; i < 30; i++)
+//        Board board = new Board(15,15);
+//        VisualizerAscii v = new VisualizerAscii(board);
+//
+//
+//        List<Integer> list= Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9);
+//        for(int i = 0; i < 30; i++)
 //        board.placeSummingField();
-        v.display();
+//        v.display();
+
+        Menu menu = new Menu();
+        menu.setVisible(true);
 
     }
 }
